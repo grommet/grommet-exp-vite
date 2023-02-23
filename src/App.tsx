e6aad83c191f1e47aebac3a108b2a9ac0ce956af
Eric Soderberg <eric.soderberg@hpe.com>
2023-02-23 03:14:52 -0800

    Updated to use Heading, Text, and Paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import { useState } from 'react'
-import { Anchor } from 'grommet-exp';
+import { Anchor, Heading, Paragraph, Text } from 'grommet-exp';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <h1>Vite + React</h1>
-      <Anchor href="">HI</Anchor>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Heading level={1}>Grommet exploration sample app</Heading>
+      <Paragraph>React + Vanilla Extract + Vite</Paragraph>
+      <Anchor href="">Anchor</Anchor>
+      <Text>Text</Text>
+      <Text size="small">small Text</Text>
     </div>
   )
 }
