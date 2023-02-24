96fb8b0cb5b6e00dc394d73ffc60ec3c78407eeb
Taylor Seamans <taylor.seamans@yahoo.com>
2023-02-24 15:44:49 -0800

    Add Grommet to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import { Anchor, Box, Button, Heading, Paragraph, Text } from "grommet-exp";
-import { themeClass, vars } from "grommet-exp-theme";
+import {
+  Anchor,
+  Box,
+  Button,
+  Grommet,
+  Heading,
+  Paragraph,
+  Text,
+} from "grommet-exp";
 
 function App() {
   return (
     <div className="App">
-      <Heading level={1}>Grommet exploration sample app</Heading>
-      <Paragraph>React + Vanilla Extract + Vite</Paragraph>
-      <Box gap="medium">
-        <Anchor href="">Anchor</Anchor>
-        <Text>Text</Text>
-        <Text size="small">small Text</Text>
-        <Button label="Test primary button" kind="primary" />
-      </Box>
+      <Grommet>
+        <Heading level={1}>Grommet exploration sample app</Heading>
+        <Paragraph>React + Vanilla Extract + Vite</Paragraph>
+        <Box gap="medium">
+          <Anchor href="">Anchor</Anchor>
+          <Text>Text</Text>
+          <Text size="small">small Text</Text>
+          <Button label="Test primary button" kind="primary" />
+        </Box>
+      </Grommet>
     </div>
   );
 }
