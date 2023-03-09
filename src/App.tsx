8bb67aa959107b52ad6b8a8e7c19b509d1aba518
Eric Soderberg <eric.soderberg@hpe.com>
2023-03-09 14:17:40 -0800

    Use Icon and Nav

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import {
   Button,
   Footer,
   Grommet,
+  Header,
+  Icon,
+  Nav,
   Page,
   PageContent,
   PageHeader,
@@ -11,10 +14,32 @@ import {
   Text,
 } from "grommet-exp";
 
+import hpe from "grommet-icons/img/hpe.svg";
+import notification from "grommet-icons/img/notification.svg";
+import helpOption from "grommet-icons/img/help-option.svg";
+import appsRounded from "grommet-icons/img/apps-rounded.svg";
+
 function App() {
   return (
     <Grommet>
       <Page kind="narrow">
+        <Header pad="small" border="bottom">
+          <Box direction="row" align="center" gap="small">
+            <Icon size="xlarge" src={hpe} />
+            <Text>HPE GreenLake</Text>
+          </Box>
+          <Nav direction="row" justify="center">
+            <Button label="Dashboard" />
+            <Button label="Applications" />
+            <Button label="Devices" />
+            <Button label="Manage" />
+          </Nav>
+          <Box direction="row" align="center" gap="small">
+            <Button icon={<Icon src={notification} />} />
+            <Button icon={<Icon src={helpOption} />} />
+            <Button icon={<Icon src={appsRounded} />} />
+          </Box>
+        </Header>
         <PageContent>
           <PageHeader
             title="Grommet Experiment"
@@ -27,7 +52,7 @@ function App() {
             <Text size="small">small Text</Text>
             <Button label="Test primary button" kind="primary" />
           </Box>
-          <Footer margin={{ vertical: 'medium' }}>
+          <Footer margin={{ vertical: "medium" }}>
             <Text size="small" color="weak">
               a work in progress
             </Text>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import image from '@rollup/plugin-image';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [image(), react(), vanillaExtractPlugin()],
 })
